2fd13795d4a3d8ed089195a5ba2efe4c29c23c85
sujithlion <sujithmogaveera27@gmail.com>
2025-11-22 07:13:50 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -170,6 +170,5 @@ def update_location():
         return jsonify({'success': True})
     
     return jsonify({'success': False})
-
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+  app.run(debug=True, host='0.0.0.0', port=5000)
